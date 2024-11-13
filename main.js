b4f390f782eee825ea3887dc54f374c8174f8514
RobTrb <robert.trb@gmail.com>
2024-11-13 23:03:06 +0100

    added createRooms function not done yet

--- main.js
+++ main.js
@@ -16,6 +16,53 @@ mainClose.addEventListener("click", () => {
 }); 
 
 async function getAPI(){
-    const response = await('https://lernia-sjj-assignments.vercel.app/api/challanges');
-    const date = await response.json();
+    const response = await fetch('https://lernia-sjj-assignments.vercel.app/api/challenges');
+    const data = await response.json();
+
+    data.challenges.forEach(console.log(data.challenges))
+
+}
+
+
+async function createRooms(){
+    const response = await fetch('https://lernia-sjj-assignments.vercel.app/api/challenges');
+    const data = await response.json();
+    const roomContainer = document.querySelector(".book__div")
+
+    data.challenges.forEach((challenge) => {
+        const roomTile = document.createElement("div");
+        roomTile.className = "book__div__room";
+        roomContainer.appendChild(roomTile);
+
+        const roomName = document.createElement("h2");
+        roomName.className = "dook__div__titel";
+        roomTile.appendChild(roomName);
+        roomName.innerHTML = challenge.title + "(" + challenge.type + ")";
+
+        const img = document.createElement("img");
+        img.className = "book__div__img";
+        img.src = challenge.image;
+        roomTile.appendChild(img);
+
+        const participants = document.createElement("p");
+        participants.className = "book__div__participants"
+        participants.innerHTML = challenge.minParticipants + "-" + challenge.maxParticipants + " participants"
+        roomTile.appendChild(participants);
+
+        const description = document.createElement("p");
+        description.className = "book__div__text";
+        description.innerHTML = challenge.description;
+        roomTile.appendChild(description);
+
+        const bookBtn = document.createElement("a");
+        bookBtn.className = "red__link"
+        roomTile.appendChild(bookBtn);
+        if (challenge.type == "online"){
+            bookBtn.innerHTML == "Take challenge online"
+        } 
+        else 
+        {
+            bookBtn.innerHTML = "Book this room"
+        }        
+    });
 }
