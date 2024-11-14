32d4f62601200c7406fdab25452d066a9c83935b
Kai <kai.ericson@lerniastudent.se>
2024-11-14 10:23:26 +0100

    sorted challenges by rating

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const mainLink = document.querySelector(".main__open");
 const menu = document.querySelector(".main__nav");
 
+setHomePageCards();
 
 mainLink.addEventListener("click", (e) => {
 
@@ -18,4 +19,14 @@ mainClose.addEventListener("click", () => {
 async function getAPI(){
     const response = await('https://lernia-sjj-assignments.vercel.app/api/challanges');
     const date = await response.json();
+}
+
+async function setHomePageCards(){
+    const response = await fetch('https://lernia-sjj-assignments.vercel.app/api/challenges');
+    const data = await response.json();
+   
+    data.challenges.sort((a,b) =>b.rating - a.rating);
+    data.challenges.forEach(challenge => {
+        console.log(challenge.rating);
+    });
 }
