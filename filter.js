044aa889fa36da340f5922a4ceedc6a00d4a9732
Louise <emmalouisekarlsson@hotmail.com>
2024-11-14 20:05:41 +0100

    JS function to mark stars for rating

--- filter.js
+++ filter.js
@@ -2,21 +2,54 @@ const closeButton = document.querySelector(".alternative__close");
 const filterMenu = document.querySelector(".filter__alternative");
 
 closeButton.addEventListener("click", () => {
-    console.log("click");
     filterMenu.setAttribute("class", "close");
     openButton.setAttribute("class", "filter__button");
-}); 
+});
 
 const openButton = document.querySelector(".filter__button")
 
 openButton.addEventListener("click", () => {
-    
+
     filterMenu.setAttribute("class", "filter__alternative--open");
-    openButton.setAttribute("class", "close");  
+    openButton.setAttribute("class", "close");
 });
 
-const star = document.querySelector(".rating__star");
-star.addEventListener("click", ()=>{
+const starFrom = document.querySelectorAll(".star__from");
 
-    star.classList.toggle("rating__star--filled"); 
+starFrom.forEach((star, index) => {
+    star.addEventListener("click", () => {
+        starFrom.forEach((star, secondIndex) => {
+            if (index >= secondIndex) {
+                star.classList.add("rating__star--filled");
+            } else {
+                star.classList.remove("rating__star--filled");
+            }
+        });
+    });
+});
+
+const starTo = document.querySelectorAll(".star__to");
+
+starTo.forEach((stars, place) => {
+    stars.addEventListener("click", () => {
+        console.log(place); 
+        starTo.forEach((stars, secondPlace) => {
+            if(place == 0){
+                const check = stars.getAttribute("class");
+                console.log(check);
+                if(check == "rating__star star__to fa-solid fa-star rating__star--filled"){
+                    stars.classList.remove("rating__star--filled"); 
+                }
+                else if(place >= secondPlace){
+                    stars.classList.add("rating__star--filled");
+                }
+            }
+            else if(place >= secondPlace){
+                stars.classList.add("rating__star--filled");
+            }else{
+                stars.classList.remove("rating__star--filled");
+            }
+        });
+    });
 }); 
+
