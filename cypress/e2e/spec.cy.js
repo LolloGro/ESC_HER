b9331943cf2e48d9d79067030a79dfaf61754454
Jan Elia <jan.elia995@hotmail.com>
2025-03-06 15:21:36 +0100

    "Setting up a cypress test"

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,13 +1,12 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.viewport(1780, 800)
-    cy.visit('http://127.0.0.1:5500/index.html')
-    cy.get('a').contains('Play online').click();
-  })
-  it('passes', () => {
-    cy.viewport(1780, 800)
-    cy.visit('http://127.0.0.1:5500/index.html')
-    cy.get('a').contains('Play online').click();
-    cy.get('button').contains('Filter challenges').click(); 
-  })
-})
+describe("Homepage Test", () => {
+  it("The test will load the homepage and check h1 content", () => {
+    
+    cy.visit("http://127.0.0.1:5500/");
+
+    // Verify the page loads correctly
+    cy.url().should("include", "5500");
+
+    // Check if <h1> contains the expected text
+    cy.get("h1").should("contain.text", "Hacker Escape Rooms");
+  });
+});
